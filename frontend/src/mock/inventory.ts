9684bd2f289ae3307eb1bc404b4e6d68896ec864
vishdevs <vishalshende0101@gmail.com>
2025-12-06 00:13:52 +0530

    Create  inventory.ts

---
+++ frontend/src/mock/inventory.ts
@@ -0,0 +1,18 @@
+export const mockProducts = [
+  {
+    id: 1,
+    name: "Dell Inspiron Laptop",
+    category: "Electronics",
+    buyingPrice: 48000,
+    sellingPrice: 55000,
+    stockQuantity: 8,
+  },
+  {
+    id: 2,
+    name: "Samsung Galaxy A55",
+    category: "Mobile",
+    buyingPrice: 25000,
+    sellingPrice: 28999,
+    stockQuantity: 15,
+  }
+];
