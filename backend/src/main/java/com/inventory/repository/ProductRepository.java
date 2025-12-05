b706c5cfc08cea9a0fa562ec7fde6b802285d296
vishdevs <vishalshende0101@gmail.com>
2025-12-06 03:57:06 +0530

    Create ProductRepository.java

---
+++ backend/src/main/java/com/inventory/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.inventory.repository;
+
+import com.inventory.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByActiveTrue();
+
+}
